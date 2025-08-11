4908def98c07d521ae1e6aea6e037f461e023ebd
JaeminSeo <115198558+seojaemin22@users.noreply.github.com>
2025-08-11 13:07:17 +0900

    Add files via upload

--- neuralop/losses/__init__.py
+++ neuralop/losses/__init__.py
@@ -2,3 +2,4 @@ from .data_losses import LpLoss, H1Loss
 from .equation_losses import BurgersEqnLoss, ICLoss
 from .meta_losses import WeightedSumLoss, FieldwiseAggregatorLoss, Aggregator, Relobralo, SoftAdapt
 from .fourier_diff import fourier_derivative_1d
+from .VINO_loss import VinoDarcyLoss
